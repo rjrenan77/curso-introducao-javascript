6ba044eb7e26a23854e8892ef22ab7cc2a5e1110
Renan <developer.renansoares@gmail.com>
2018-09-04 10:43:44 -0300

    adição de for preencher toda a tabela com calculo da imc e mudança de css conforme boas práticas

--- js/principal.js
+++ js/principal.js
@@ -1,40 +1,59 @@
  // console.log("fui carregado de arquivo externo!");
-  var titulo = document.querySelector(".titulo");
-  titulo.textContent = "Aparecida Nutricionista";
+ var titulo = document.querySelector(".titulo");
+ titulo.textContent = "Aparecida Nutricionista";
 
-  var paciente = document.querySelector("#primeiro-paciente");
+ var pacientes = document.querySelectorAll(".paciente");
+
+
+ for(var i = 0; i < pacientes.length; i++){
+
+  console.log(pacientes[i]);
+
+  var paciente = pacientes[i];
+
   var tdPeso = paciente.querySelector(".info-peso");
   var peso = tdPeso.textContent;
+  console.log(peso);
 
-  var tdAltura = document.querySelector(".info-altura");
+
+
+  var tdAltura = paciente.querySelector(".info-altura");
   var altura = tdAltura.textContent;
+  console.log(altura);
 
-  var tdImc = document.querySelector(".info-imc");
+  var tdImc = paciente.querySelector(".info-imc");
 
   var pesoEhValido = true;
   var alturaEhValida = true;
 
   if(peso<= 0 || peso>=1000){
-  	console.log("peso inválido");
-  	pesoEhValido = false;
-  	tdPeso.textContent = "Peso Inválido";
+    console.log("peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+    paciente.classList.add("paciente-invalido");
   }
 
   if(altura<=0 || altura>=3.00){
-  	console.log("altura inválida");
-  	alturaEhValida = false;
-  	tdAltura.textContent = "Altura inválida"
+    console.log("altura inválida");
+    alturaEhValida = false;
+    tdAltura.textContent = "Altura inválida"
+    paciente.classList.add("paciente-invalido");
   }
 
   if(alturaEhValida && pesoEhValido){
-  	var imc = peso/(altura*altura);	
-  	console.log("O imc é = " + imc);
+    var imc = peso/(altura*altura); 
+    console.log("O imc é = " + imc);
+    tdImc.textContent = imc.toFixed(2);
   }
 
-  
+}
 
-  var tdImc = document.querySelector(".info-imc");
-  tdImc.textContent = imc; //alterando o valor da tabela imc de forma dinamica
+
+
+
+
+//var tdImc = document.querySelector(".info-imc");
+  //tdImc.textContent = imc; //alterando o valor da tabela imc de forma dinamica
 
   //console.log(paciente);
   //console.log(tdPeso);
