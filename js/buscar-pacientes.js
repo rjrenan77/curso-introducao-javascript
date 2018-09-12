d47def0a6be909309378d83aed313de99c67a9f7
Renan <developer.renansoares@gmail.com>
2018-09-12 19:06:44 -0300

    consertando mensagem de busca erronea

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -4,23 +4,23 @@ botaoAdicionar.addEventListener("click",function(){
 	//console.log("buscando pacientes ");
 
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientesa");
+	xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
 
 	xhr.addEventListener("load", function(){
-	var erroAjax = document.querySelector("erro-ajax");
+		var erroAjax = document.querySelector("#erro-ajax");
 		if(xhr.status == 200){
 			erroAjax.classList.add("invisivel");
 			var resposta = xhr.responseText;
 			//console.log(typeof resposta);
-			console.log(resposta);
-			console.log(typeof resposta);
+			//console.log(resposta);
+			//console.log(typeof resposta);
 			var pacientes = JSON.parse(resposta);
 			//console.log(pacientes);
 			//console.log(typeof pacientes);
 
 			pacientes.forEach(function(paciente){
 				adicionaPacienteNaTabela(paciente);
-		
+
 			});
 		}else{
 			console.log(xhr.status);
@@ -30,7 +30,7 @@ botaoAdicionar.addEventListener("click",function(){
 
 		}
 
-});
+	});
 	xhr.send();
 
 });
